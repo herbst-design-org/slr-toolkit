0c4a84ea08b5b7070c3e3ba8fb695c9fdaea85b8
paul-hbst <p2h4go@gmail.com>
2025-05-27 11:08:51 +0200

    fixed more type errors

--- src/app/slr/[id]/page.tsx
+++ src/app/slr/[id]/page.tsx
@@ -8,23 +8,23 @@ import ManageItems from "./ManageItems";
 import { Subheading } from "~/app/_components/heading";
 import { Divider } from "~/app/_components/divider";
 
-export default async function SLRPage({ params }: { params: { id: string } }) {
-  const { id } = await params
-  const slr = await api.slr.getById({ id });
-  const providers = await api.contentProvider.getAll();
-  if (!slr) redirect("/");
-  return (
-    <CustomNavbar title={slr?.title ?? "unknown"}>
-      <div className="flex flex-grow w-full justify-between border p-8 mb-12 border-zinc-700">
-        <div className="md:w-64">
-          <Subheading> Stats </Subheading>
-          <Divider />
-          <SlrInfo slr={slr} />
-        </div>
-        <ManageSubscriptions slrId={slr.id} providers={providers} />
-      </div>
-      <ManageItems slrId={slr.id} />
-      <ItemsInSLR slr={slr} />
-    </CustomNavbar>
-  );
+export default async function SLRPage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
+	const slr = await api.slr.getById({ id });
+	const providers = await api.contentProvider.getAll();
+	if (!slr) redirect("/");
+	return (
+		<CustomNavbar title={slr?.title ?? "unknown"}>
+			<div className="flex flex-grow w-full justify-between border p-8 mb-12 border-zinc-700">
+				<div className="md:w-64">
+					<Subheading> Stats </Subheading>
+					<Divider />
+					<SlrInfo slr={slr} />
+				</div>
+				<ManageSubscriptions slrId={slr.id} providers={providers} />
+			</div>
+			<ManageItems slrId={slr.id} />
+			<ItemsInSLR slr={slr} />
+		</CustomNavbar>
+	);
 }
